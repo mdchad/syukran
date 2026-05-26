21dd65a9084ab0b3decbf4ab16cf78264d84cb25
mdchad <irsyad.muhd@gmail.com>
2026-05-26 15:21:48 +0800

    added turso

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -60,6 +60,7 @@ export default buildConfig({
   db: sqliteAdapter({
     client: {
       url: process.env.DATABASE_URL || '',
+      authToken: process.env.DATABASE_AUTH_TOKEN,
     },
   }),
   collections: [Pages, Posts, Media, Categories, Users],
